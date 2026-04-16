f2ee4eaa4f29612a93b28026f8224560c6b3e351
Duniaal <bq07md@student.aau.dk>
2026-04-16 11:48:37 +0200

    Made the notation buttons in the playpage work

--- front-end/pages/sudoku-page/sudoku.js
+++ front-end/pages/sudoku-page/sudoku.js
@@ -150,3 +150,18 @@ for (var i = 0; i < 9; i++) {
 const OurSudokuBoard = new SudokuBoard(ourCellsArr); 
 
 OurSudokuBoard.setupBoard();
+
+// This is the button code, that adds the activeNotation class to the clicked button
+const buttons = document.querySelectorAll('#notation_Boxes button:not(#forfeit_btn)');
+
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+
+        // remove active from all
+        buttons.forEach((btn) => btn.classList.remove('activeNotation'));
+        
+        // add to clicked one
+        button.classList.add('activeNotation');
+    
+    });
+});
