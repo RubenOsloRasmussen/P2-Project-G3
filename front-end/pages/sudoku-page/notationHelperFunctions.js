f6936f89c024ef2eeccb49dc0e18e5cf7f980070
Stephanie Hansen <125078207+StephanieCHH@users.noreply.github.com>
2026-04-20 13:56:16 +0200

    Finished helper functions for getting the indices of cells with the same number, or in the same block, row or column

--- front-end/pages/sudoku-page/notationHelperFunctions.js
+++ front-end/pages/sudoku-page/notationHelperFunctions.js
@@ -5,6 +5,7 @@ export function findSameNumberInstances(rowIndex, columnIndex, ourCellsArr) {
     let instancesIndexArray = [];
     let number = ourCellsArr[rowIndex][columnIndex].number
 
+    // Saves the index of every cell with the same number.
     for (let i = 0; i < 9; i++) {
         for (let j = 0; j < 9; j++) {
             if (ourCellsArr[i][j].number === number) {
@@ -16,17 +17,41 @@ export function findSameNumberInstances(rowIndex, columnIndex, ourCellsArr) {
     return instancesIndexArray;
 }
 
-export function findSameBlockInstances(number, ourCellsArr) {
+export function findSameBlockInstances(rowIndex, columnIndex, ourCellsArr) {
     let instancesIndexArray = [];
 
-    for (let i = 0; i < 9; i++) {
-        for (let j = 0; j < 9; j++) {
-            if (ourCellsArr[i][j].number === number) {
-                instancesIndexArray.push(RowAndColumnToIndex(i, j));
-            }
+    // Calculate the row and column index of the top left cell in the number's block.
+    let startRowIndex = Math.floor(rowIndex / 3) * 3;
+    let startColumnIndex = Math.floor(columnIndex / 3) * 3;
+
+    // Saves the index of every cell in the block.
+    for (let i = 0; i < 3; i++) {
+        for (let j = 0; j < 3; j++) {
+            instancesIndexArray.push(RowAndColumnToIndex(startRowIndex+i, startColumnIndex+j));
         }
     }
 
     return instancesIndexArray;
 }
 
+export function findSameRowInstances(rowIndex, columnIndex, ourCellsArr) {
+    let instancesIndexArray = [];
+
+    // Saves the index of every cell in the row.
+    for (let i = 0; i < 9; i++) {
+        instancesIndexArray.push(RowAndColumnToIndex(rowIndex, i));
+    }
+
+    return instancesIndexArray;
+}
+
+export function findSameColumnInstances(rowIndex, columnIndex, ourCellsArr) {
+    let instancesIndexArray = [];
+
+    // Saves the index of every cell in the column.
+    for (let i = 0; i < 9; i++) {
+        instancesIndexArray.push(RowAndColumnToIndex(i, columnIndex));
+    }
+
+    return instancesIndexArray;
+}
