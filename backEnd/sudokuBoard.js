dd51bba3e870889d8789428ec50b80daf08fcf9b
RubenOsloRasmussen <ruben@oslonet.dk>
2026-04-30 12:59:51 +0200

    fixed error in initialisation

--- backEnd/sudokuBoard.js
+++ backEnd/sudokuBoard.js
@@ -2,9 +2,6 @@ import fs from "fs";
 import path from "path";
 import { fileURLToPath } from "url";
 
-import { merge } from "./helperfunctions.js";
-import { mergeSort } from "./helperfunctions.js";
-
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
@@ -13,10 +10,10 @@ const CSV_PATH = path.join(__dirname, "SudokuPuzzles.csv");
 export function GetSudokuBoard(sudokuNumber) {
     const csv = fs.readFileSync(CSV_PATH, "utf-8");
     
-    if (!sudokuString) {
-        console.log("Could not load Sudoku string")
-        return;
-    }
+    //if (!csv.ok) {
+    //    console.log("Could not load Sudoku string")
+    //    return;
+    //}
     const puzzles = csv.split(",");
 
     let stringArr = [];
@@ -59,4 +56,57 @@ function stringToBoard(sudokuString) {
     }
 
     return board;
+}
+
+function merge(arr, left, mid, right) {
+    const n1 = mid - left + 1;
+    const n2 = right - mid;
+
+    const lArr = new Array(n1);
+    const rArr = new Array(n2);
+
+    for (var i = 0; i < n1; i++) {
+        lArr[i] = arr[left + i]
+        lArr[i][0] = Number(lArr[i][0]);
+    }
+    for (var j = 0; j < n2; j++) {
+        rArr[j] = arr[mid + 1 + j]
+        rArr[j][0] = Number(rArr[j][0]);
+    }
+
+    i = 0, j = 0;
+    let k = left;
+
+    while (i < n1 && j < n2) {
+        if (lArr[i][0] <= rArr[j][0]) {
+            arr[k] = lArr[i];
+            i++;
+        } else {
+            arr[k] = rArr[j];
+            j++;
+        }
+        k++;
+    }
+
+    while (i < n1) {
+        arr[k] = lArr[i];
+        i++;
+        k++;
+    }
+
+    while (j < n2) {
+        arr[k] = rArr[j];
+        j++;
+        k++;
+    }
+}
+
+function mergeSort(arr, left, right) {
+    if (left >= right)
+        return;
+
+    const mid = Math.floor(left + (right - left) / 2);
+    mergeSort(arr, left, mid);
+    mergeSort(arr, mid + 1, right);
+    merge(arr, left, mid, right);
 }
